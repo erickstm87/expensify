fefbe5aef519c467015038f554bb5fa103ed5ba0
erickstm87 <erickstm87@github.com>
2018-03-22 06:05:55 -0600

    still troubleshooting add expenses

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { addExpense } from '../actions/expenses'
+import { startAddExpenses } from '../actions/expenses'
 
 export class AddExpensePage extends React.Component {
     onSubmit = (expense) => {
         //props.dispatch(addExpense(expense));
-        this.props.addExpense(expense);
+        this.props.startAddExpenses(expense);
         this.props.history.push('/');
     };
     render(){
@@ -19,7 +19,7 @@ export class AddExpensePage extends React.Component {
     }
 }
 const mapDispatchToProps = (dispatch) => ({ 
-    addExpense: (expense) => dispatch(addExpense(expense)) 
+    startAddExpenses: (expense) => dispatch(startAddExpenses(expense)) 
 });
 
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,108 +12,110 @@ const config = {
 firebase.initializeApp(config);
 const database = firebase.database();
 
-database.ref('expenses')
-    .once('value')
-    .then((snapshot) => {
-        let newArray = [];
-        snapshot.forEach((childSnapshot) => {
-            newArray.push({
-                id: childSnapshot.key,
-                ...childSnapshot.val()
-            });
-            console.log(newArray);
-        });
-    })
-    .catch((e) => {
-        console.log('error was created: ', e)
-    })  
+export { firebase, database as default };
 
-
-database.ref('expenses').on('child_removed', (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-});
-
-database.ref('expenses').on('child_changed', (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-});
-
-database.ref('expenses').on('child_added', (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-});
-
-// database.ref('expenses').push({
-//     description: 'rent',
-//     note: 'movin out to the big city!',
-//     amount: 11,
-//     createdAt: '0'
-// });
-
-// database.ref().on('value', (snapshot) => {
-//     console.log(snapshot.val());
-// });
-
-// database.ref().on('value', (snapshot) => {
-//     const val = snapshot.val();
-//     console.log(`${val.name} is a ${val.job} and is this many years old, ${val.age}`);
-// }, (e) => {
-//     console.log('error: ', e)
-// })
-
-// database.ref('expenses').on('value', (snapshot) => {
-//     let newArray = [];
-//     snapshot.forEach((childSnapshot) => {
-//         newArray.push({
-//             id: childSnapshot.key,
-//             ...childSnapshot.val()
+// database.ref('expenses')
+//     .once('value')
+//     .then((snapshot) => {
+//         let newArray = [];
+//         snapshot.forEach((childSnapshot) => {
+//             newArray.push({
+//                 id: childSnapshot.key,
+//                 ...childSnapshot.val()
+//             });
+//             console.log(newArray);
 //         });
-//         console.log(newArray);
-//     });
+//     })
+//     .catch((e) => {
+//         console.log('error was created: ', e)
+//     })  
+
+
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
 // });
 
-// database.ref().once('value')
-//         .then((snapshot) => {
-//             const val = snapshot.val();
-//             console.log(val);
-//         })
-//         .catch((e) => {
-//             console.log('error: ', e);
-//         })
-
-// database.ref().set({
-//     name: 'tomas',
-//     age: 30,
-//     isSingle: false,
-//     location: {
-//         city: 'boulder',
-//         country: 'USA',
-//         hometown: 'milwaukee'
-//     }
-// }).then(() => {
-//     console.log('data is saved');
-// }).catch((e) => {
-//     console.log('error message is: ', e);
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
 // });
 
-// database.ref('age').set(31);
-// database.ref('location/city').set('denver');
-
-//attributes challenge: store their eye color and hair color
-
-// database.ref('attributes').set({
-//     eye_color: 'blue',
-//     hair_color: 'brown'
-// }).then(() => {
-//     console.log('changed attributes successfully');
-// }).catch((e) => {
-//     console.log('error here: ', e);
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
 // });
 
-// database.ref('isSingle').remove();
-// // database.ref('isSingle').set(null); <-- This is an alternative way to remove data
-// database.ref().update({
-//     name: 'Felipe',
-//     age: 45,
-//     'location/country': 'world',
-//     'job': 'software man'
+// // database.ref('expenses').push({
+// //     description: 'rent',
+// //     note: 'movin out to the big city!',
+// //     amount: 11,
+// //     createdAt: '0'
+// // });
 
-// });
+// // database.ref().on('value', (snapshot) => {
+// //     console.log(snapshot.val());
+// // });
+
+// // database.ref().on('value', (snapshot) => {
+// //     const val = snapshot.val();
+// //     console.log(`${val.name} is a ${val.job} and is this many years old, ${val.age}`);
+// // }, (e) => {
+// //     console.log('error: ', e)
+// // })
+
+// // database.ref('expenses').on('value', (snapshot) => {
+// //     let newArray = [];
+// //     snapshot.forEach((childSnapshot) => {
+// //         newArray.push({
+// //             id: childSnapshot.key,
+// //             ...childSnapshot.val()
+// //         });
+// //         console.log(newArray);
+// //     });
+// // });
+
+// // database.ref().once('value')
+// //         .then((snapshot) => {
+// //             const val = snapshot.val();
+// //             console.log(val);
+// //         })
+// //         .catch((e) => {
+// //             console.log('error: ', e);
+// //         })
+
+// // database.ref().set({
+// //     name: 'tomas',
+// //     age: 30,
+// //     isSingle: false,
+// //     location: {
+// //         city: 'boulder',
+// //         country: 'USA',
+// //         hometown: 'milwaukee'
+// //     }
+// // }).then(() => {
+// //     console.log('data is saved');
+// // }).catch((e) => {
+// //     console.log('error message is: ', e);
+// // });
+
+// // database.ref('age').set(31);
+// // database.ref('location/city').set('denver');
+
+// //attributes challenge: store their eye color and hair color
+
+// // database.ref('attributes').set({
+// //     eye_color: 'blue',
+// //     hair_color: 'brown'
+// // }).then(() => {
+// //     console.log('changed attributes successfully');
+// // }).catch((e) => {
+// //     console.log('error here: ', e);
+// // });
+
+// // database.ref('isSingle').remove();
+// // // database.ref('isSingle').set(null); <-- This is an alternative way to remove data
+// // database.ref().update({
+// //     name: 'Felipe',
+// //     age: 45,
+// //     'location/country': 'world',
+// //     'job': 'software man'
+
+// // });
