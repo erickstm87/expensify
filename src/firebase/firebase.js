9c91f6454bcce0c30f97aa5ffc1242804b650851
erickstm87 <erickstm87@github.com>
2018-03-26 16:41:17 -0600

    done with test for db

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyABnxkYXWwYmUEFxhH-CnN_D5hHr1FdpX0",
-    authDomain: "expensify-c1a74.firebaseapp.com",
-    databaseURL: "https://expensify-c1a74.firebaseio.com",
-    projectId: "expensify-c1a74",
-    storageBucket: "expensify-c1a74.appspot.com",
-    messagingSenderId: "245741626365"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGINGS_SENDER_ID
 };
 
 firebase.initializeApp(config);
