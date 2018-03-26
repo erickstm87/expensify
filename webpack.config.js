7664b576de8feabb6ae85dd5f2741c1fe8107ca7
erickstm87 <erickstm87@github.com>
2018-03-26 17:36:25 -0600

    forgot the process keyword

--- webpack.config.js
+++ webpack.config.js
@@ -55,8 +55,8 @@ module.exports = (env) => {
                 'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
                 'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
                 'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(FIREBASE_STORAGE_BUCKET),
-                'process.env.FIREBASE_MESSAGINGS_SENDER_ID': JSON.stringify(FIREBASE_MESSAGINGS_SENDER_ID)
+                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.env.FIREBASE_MESSAGINGS_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGINGS_SENDER_ID)
             })
         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
