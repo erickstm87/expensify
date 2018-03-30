47a5c594603b5dad4b2f3bd30d9bb0a2c8addb62
Thomas Erickson <terickson@spotx.tv>
2018-03-30 09:35:54 -0600

    middle of history module

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import createHistory from 'history/createBrowserHistory';
 import { Switch, BrowserRouter, Route, Link, NavLink} from 'react-router-dom';
 import ExpenseDashboardPage from '../components/HomePage';
 import AddExpensePage from '../components/AddExpense';
@@ -8,20 +9,24 @@ import NotFound from '../components/NotFound';
 import Header from '../components/Header';
 import LoginPage from '../components/LoginPage';
 
+const history = createHistory();
+
 const AppRouter = () => (
-    <BrowserRouter> 
-        <div>
-            <Header />
-            <Switch>
-                <Route path='/' component={LoginPage} exact={true}/>
-                <Route path='/dashboard' component={ExpenseDashboardPage}/>
-                <Route path='/create' component={AddExpensePage}/>
-                <Route path='/edit/:id' component={EditExpensePage}/>
-                <Route path='/help' component={HelpPage}/>
-                <Route component={NotFound}/>
-            </Switch>  
-        </div> 
-    </BrowserRouter>
+    <Router history={history}>
+        <BrowserRouter> 
+            <div>
+                <Header />
+                <Switch>
+                    <Route path='/' component={LoginPage} exact={true}/>
+                    <Route path='/dashboard' component={ExpenseDashboardPage}/>
+                    <Route path='/create' component={AddExpensePage}/>
+                    <Route path='/edit/:id' component={EditExpensePage}/>
+                    <Route path='/help' component={HelpPage}/>
+                    <Route component={NotFound}/>
+                </Switch>  
+            </div> 
+        </BrowserRouter>
+    </Router>
 );
  
 export default AppRouter;
