f48557c717dad4d66897f71658fed22df0dc46ec
Thomas Erickson <terickson@spotx.tv>
2018-03-30 08:25:40 -0600

    Login and logout with tests

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,6 +1,6 @@
 //react-test-renderer
 import React from 'react';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 
@@ -9,6 +9,14 @@ test('should render header correctly', () => {
     // renderer.render(<Header />);
     // expect(renderer.getRenderOutput()).toMatchSnapshot();
     //expect(wrapper.find('h1').text()).toBe('Expensify');
-    const wrapper = shallow(<Header />);
+    const wrapper = shallow(<Header startLogout={() => { }}/>);
     expect(toJSON(wrapper)).toMatchSnapshot();
 });
+
+test('should call startLogout on button click', () => {
+    const startLogout = jest.fn();
+    const wrapper = shallow(<Header startLogout={startLogout}/>);
+    wrapper.find('button').simulate('click');
+    expect(startLogout).toHaveBeenCalled();
+});
+
