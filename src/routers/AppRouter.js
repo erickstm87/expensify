467c438e058db9fb6772570d7cdfdaa185266cf4
Thomas Erickson <terickson@spotx.tv>
2018-03-30 08:12:14 -0600

    setup google auth

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,13 +6,15 @@ import EditExpensePage from '../components/EditExpensePage'
 import HelpPage from '../components/HelpPage';
 import NotFound from '../components/NotFound';
 import Header from '../components/Header';
+import LoginPage from '../components/LoginPage';
 
 const AppRouter = () => (
     <BrowserRouter> 
         <div>
             <Header />
             <Switch>
-                <Route exact path='/' component={ExpenseDashboardPage}/>
+                <Route path='/' component={LoginPage} exact={true}/>
+                <Route path='/dashboard' component={ExpenseDashboardPage}/>
                 <Route path='/create' component={AddExpensePage}/>
                 <Route path='/edit/:id' component={EditExpensePage}/>
                 <Route path='/help' component={HelpPage}/>
