d046c62ca21bdd27ebc5f1f6bf779cca0222287f
erickstm87 <erickstm87@github.com>
2018-04-04 21:44:20 -0600

    fixed the hidden css bug

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,7 +4,7 @@ import createHistory from 'history/createBrowserHistory';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpense';
 import EditExpensePage from '../components/EditExpensePage';
-import NotFoundPage from '../components/NotFound';
+import NotFound from '../components/NotFound';
 import LoginPage from '../components/LoginPage';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
@@ -17,7 +17,7 @@ const AppRouter = () => (
       <Switch>
         <PublicRoute path="/" component={LoginPage} exact={true} />
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
-        <PrivateRoute path="/create" component={AddExpense} />
+        <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
         <Route component={NotFound} />
       </Switch>
