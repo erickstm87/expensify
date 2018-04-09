9cf894889ed699d4b7466d4e359a1792b620be99
Thomas Erickson <terickson@spotx.tv>
2018-04-08 19:53:29 -0600

    further stylings for desktop and mobile

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -4,16 +4,13 @@ import moment from 'moment';
 import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
+    <Link to={`/edit/${id}`}>
     <div>
-        <Link to={`/edit/${id}`}>
-            <h3>{description}</h3>
-        </Link>
-        <p> 
-            {numeral(amount/100).format('$0,0.00')}
-            -
-            {moment(createdAt).format('MMMM Do, YYYY')}
-        </p>
+        <h3>{description}</h3>
+        <span>{moment(createdAt).format('MMMM Do, YYYY')}</span>
     </div>
+    <h3>{numeral(amount/100).format('$0,0.00')}</h3>
+    </Link>
 );
 
 export default ExpenseListItem;
